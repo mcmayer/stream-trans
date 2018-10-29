26a67cef20ab04f0e7af15c6c5381a0a13871c42
MM <mmayer@mayeranalytics.com>
2018-10-29 17:07:52 +0100

    lts-12.16

--- Data/Vector/Fusion/Stream/Monadic/Bitstream.hs
+++ Data/Vector/Fusion/Stream/Monadic/Bitstream.hs
@@ -21,17 +21,19 @@
 
 module Data.Vector.Fusion.Stream.Monadic.Bitstream
 (
-    Bitstream, Bitstream', mkBitstream, mkByteString, mkByteString'
+    Bitstream, Bitstream', mkBitstream, mkByteString, mkByteString',
+    bitstreamToWordNStream, bitstreamToBytestream
 ) where
 
 import           Control.Monad.Identity
 import           Data.Bits                               (shiftL, shiftR, (.&.),
-                                                          (.|.))
+                                                          (.|.), Bits)
 import qualified Data.ByteString.Lazy                    as BSL
 import           Data.Vector.Fusion.Stream.Monadic       (Step (..))
 import qualified Data.Vector.Fusion.Stream.Monadic       as S
 import           Data.Vector.Fusion.Stream.Monadic.Trans (get, (>>>))
 import           Data.Word8                              (Word8)
+import           Data.Word                               (Word16, Word32, Word64)
 
 -- |This is used in `mkBitstream`. The first `Word8` is the current byte,
 -- the second `Word8` is the remaining (unconsumed) bits - 1.
@@ -55,31 +57,58 @@ mkBitstream bs' = S.Stream step (StepBitStr bs' 0 0) where
 
 {-# INLINE_FUSED byteStreamToByteString #-}
 byteStreamToByteString :: Monad m => S.Stream m Word8 -> m BSL.ByteString
-byteStreamToByteString s = S.foldr BSL.cons mempty s
+byteStreamToByteString = S.foldr BSL.cons mempty
 
-data Iter s = Iter !s !Word8 !Int
+-- |Type for iterating over state s, yielding i, with Int counter
+data IterI i s = IterI !s !i !Int
 
-{-# INLINE_FUSED readByte #-}
-readByte :: Monad m => (s -> m (S.Step s Bool)) -> (s -> m (S.Step s Word8))
-readByte step = stepw where
-    {-# INLINE_INNER stepw #-}
-    stepw s = do
-        Iter s' w' n' <- f step (Iter s 0 7)
-        return $ if n' == 0 then Yield w' s' else Done
-        where
-            f :: Monad m => (s -> m (S.Step s Bool)) -> Iter s -> m (Iter s)
-            f step' (Iter s' w n) = do
-                stp <- step' s'
-                case stp of
-                    Done -> return (Iter s' w n)
-                    Skip s'' ->  f step' (Iter s'' w n)
-                    Yield b s'' -> if n == 0 then return (Iter s'' w' 0) else f step' (Iter s'' w' (n-1))
-                        where w' = w .|. shiftL (if b then 1 else 0) (7-n)
-            {-# INLINE_INNER f #-}
+{-# INLINE_FUSED readWordN #-}
+-- |Read n bits yielding a 'word' of iType, which must be in class Integral and Bits
+-- |It is the user's job to ensure that nBits <= bitlength(iType) !
+readWordN :: (Monad m, Bits iType, Integral iType) => Int -> (s -> m (S.Step s Bool)) -> (s -> m (S.Step s iType))
+readWordN nBits step s = do
+    IterI s' w' n' <- advance step (IterI s 0 (nBits-1))
+    return $ if n' == 0 then Yield w' s' else Done
+    where
+        advance :: (Monad m, Bits iType, Integral iType) => (s -> m (S.Step s Bool)) -> IterI iType s -> m (IterI iType s)
+        advance step' (IterI s' w n) = do
+            stp <- step' s'
+            case stp of
+                Done        -> return (IterI s' w n)
+                Skip s''    -> advance step' (IterI s'' w n)
+                Yield b s'' -> if n == 0 then return (IterI s'' w' 0) else advance step' (IterI s'' w' (n-1))
+                    where w' = w .|. shiftL (if b then 1 else 0) (nBits-1-n)
+        {-# INLINE_INNER advance #-}
+
+readWord8 :: Monad m => (s -> m (S.Step s Bool)) -> (s -> m (S.Step s Word8))
+readWord8 = readWordN 8
+
+{-# INLINE_FUSED writeWordN #-}
+-- |Write a 'word' with n bits of type iType yielding Bools. iType must be in class Integral and Bits.
+-- |It is the user's job to ensure that nBits <= bitlength(iType) !
+writeWordN :: (Monad m, Bits iType, Integral iType) => Int -> (s -> m (S.Step s iType)) -> (s -> m (S.Step s Bool))
+writeWordN nBits step s = do
+    IterI s' w' n' <- f step (IterI s 0 (nBits-1))
+    return $ if n' == 0 then Yield w' s' else Done
+    where
+        f :: (Monad m, Bits iType, Integral iType) => (s -> m (S.Step s Bool)) -> IterI iType s -> m (IterI iType s)
+        f step' (IterI s' w n) = do
+            stp <- step' s'
+            case stp of
+                Done -> return (IterI s' w n)
+                Skip s'' ->  f step' (IterI s'' w n)
+                Yield b s'' -> if n == 0 then return (IterI s'' w' 0) else f step' (IterI s'' w' (n-1))
+                    where w' = w .|. shiftL (if b then 1 else 0) (nBits-1-n)
+        {-# INLINE_INNER f #-}
+
 
 {-# INLINE_FUSED bitstreamToBytestream #-}
 bitstreamToBytestream :: Monad m => Bitstream m -> S.Stream m Word8
-bitstreamToBytestream (S.Stream step s0) = S.Stream (readByte step) s0
+bitstreamToBytestream (S.Stream step s0) = S.Stream (readWord8 step) s0
+
+{-# INLINE_FUSED bitstreamToWordNStream #-}
+bitstreamToWordNStream :: (Monad m, Bits i, Integral i) => Int -> Bitstream m -> S.Stream m i
+bitstreamToWordNStream n (S.Stream step s0) = S.Stream (readWordN n step) s0
 
 {-# INLINE_INNER bitstreamToBytestream' #-}
 bitstreamToBytestream' :: Monad m => Bitstream m -> S.Stream m Word8
